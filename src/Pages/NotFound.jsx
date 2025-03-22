53636f86f7f3b9d6c219af8a44dc62c9ec0407d0
sagar05-git <sagartyagi715@gmail.com>
2025-03-23 01:15:57 +0530

    update

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+    <div className="w-full flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
       <h1 className="text-7xl font-bold text-gray-800">404</h1>
       <p className="text-xl text-gray-600 mt-2">Oops! Page not found.</p>
       <Link
